f26aae41c0ff852bbf7205d37bd5683c9439639e
Yakushin <saratov482043162@gmail.com>
2026-04-20 12:28:14 +0400

    + локаторы, покупка товара

---
+++ src/test/java/lesson2/LocatorTest.java
@@ -0,0 +1,26 @@
+package lesson2;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+
+public class LocatorTest {
+    @Test
+    public void checkLocator() {
+        ChromeOptions options = new ChromeOptions();
+        HashMap<String, Object> chromePrefs = new HashMap<>();
+        chromePrefs.put("credentials_enable_service", false);
+        chromePrefs.put("profile.password_manager_enabled", false);
+        options.setExperimentalOption("prefs", chromePrefs);
+        options.addArguments("--incognito");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--disable-popup-blocking");
+        options.addArguments("--disable-infobars");
+        WebDriver driver = new ChromeDriver(options);
+
+        driver.quit();
+    }
+}
