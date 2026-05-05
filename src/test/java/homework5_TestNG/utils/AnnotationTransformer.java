55f31d756f3e5ded4f9864b239886a9fa7b3e0ff
Yakushin <saratov482043162@gmail.com>
2026-05-05 12:04:38 +0400

    [+] AnnotationTransformer

--- src/test/java/homework5_TestNG/utils/AnnotationTransformer.java
+++ src/test/java/homework5_TestNG/utils/AnnotationTransformer.java
@@ -8,9 +8,13 @@ import java.lang.reflect.Method;
 
 public class AnnotationTransformer implements IAnnotationTransformer {
     @Override
-    public void transform(ITestAnnotation annotation, Class testClass,
-                          Constructor testConstructor, Method testMethod) {
-        // Устанавливаем Retry анализатор для каждого тестового метода
+    public void transform(ITestAnnotation annotation, Class testClass, Constructor testConstructor, Method testMethod) {
+        // Проверка нужна, чтобы игнорировать служебные вызовы TestNG, в которых нет тестового метода, и избежать NullPointerException
+        if (testMethod == null) {
+            return;
+        }
+
+        // Устанавливаем Retry анализатор для каждого тестового метода, Способы подключения Retry по (итоговая таблица которая выше)
         annotation.setRetryAnalyzer(Retry.class);
     }
 }
