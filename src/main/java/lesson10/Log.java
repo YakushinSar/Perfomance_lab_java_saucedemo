604d4bae89c03bf5d0ccb2ead0ba61cb988ba1a7
Yakushin <saratov482043162@gmail.com>
2026-05-21 19:03:04 +0400

    [+] добавление инфо по log

--- src/main/java/lesson10/Log.java
+++ src/main/java/lesson10/Log.java
@@ -7,19 +7,13 @@ public class Log {
  выполнения тестов.
 Что логировать? - Все по максимуму, чтобы понимать, что происходило в тесте не видя браузера
 Что не логировать? - Избегать логов со статичным контентом, лучше всегда логировать какие-либо переменные
+
+# Зависимости
 <!-- Source: https://mvnrepository.com/artifact/org.apache.logging.log4j/log4j-core -->
 <dependency>
     <groupId>org.apache.logging.log4j</groupId>
     <artifactId>log4j-core</artifactId>
     <version>2.26.0</version>
-    <scope>compile</scope>
-</dependency>
-
-<!-- Source: https://mvnrepository.com/artifact/tools.jackson.dataformat/jackson-dataformat-yaml -->
-<dependency>
-    <groupId>tools.jackson.dataformat</groupId>
-    <artifactId>jackson-dataformat-yaml</artifactId>
-    <version>3.0.2</version>
     <scope>compile</scope>
 </dependency>
 
@@ -33,8 +27,11 @@ public class Log {
 | **TRACE** | Максимально детально (почти никогда) |
 В автотестах чаще всего используют **INFO**, **WARN**, **ERROR**.
 
-# Настройка логирования, настраивается для логов уровня INFO - файл log4j2-test.yaml,который лежит в src/test/resources
-#https://springframework.guru/log4j-2-configuration-using-yaml/
+# Настройка логирования - существует два способа формата конфигурации:log4j2-test.yaml/log4j2.yaml или log4j2.xml
+# Способ 1: log4j2-test.yaml
+1.Создать в src/test/resources файл log4j2-test.yaml
+Ссылка https://springframework.guru/log4j-2-configuration-using-yaml/
+Пример:
 #https://logging.apache.org/log4j/2.x/manual/configuration.html
 Configuration:
   status: INFO
@@ -75,11 +72,18 @@ Configuration:
         - ref: fileAppender
           level: INFO
 
-или
-## Настройка логирования log4j2.xml
-**Важно:** Если в папке `src/test/resources` есть файлы `log4j2-test.yaml` или `log4j2.yaml`, Log4j2 использует их, а `log4j2.xml` игнорируется.
-### Куда класть файл:
-`src/test/resources/log4j2.xml`
+2. log4j2-test.yaml — для парсинга YAML файлов Log4j2 требуется дополнительная зависимость jackson-dataformat-yaml
+<!-- Source: https://mvnrepository.com/artifact/tools.jackson.dataformat/jackson-dataformat-yaml -->
+<dependency>
+    <groupId>tools.jackson.dataformat</groupId>
+    <artifactId>jackson-dataformat-yaml</artifactId>
+    <version>3.0.2</version>
+    <scope>compile</scope>
+</dependency>
+
+# Способ 2: Настройка логирования log4j2.xml
+Важно: если в папке `src/test/resources` есть файлы `log4j2-test.yaml` или `log4j2.yaml`, Log4j2 использует их, а `log4j2.xml` игнорируется.
+Куда класть файл:`src/test/resources/log4j2.xml`
 ### Основные элементы:
 - **Console** — выводит логи в консоль
 - **File** — сохраняет логи в файл
@@ -123,9 +127,9 @@ Configuration:
 </Configuration>
 
 # Использование.
-Логирование работает по капотом ламбока. Классы, которые нужно логировать помечаются аннотацией @Log4j2, а методы в пейджах
- помечаются аннотацией  log.info("Writing '{}' in to '{}'", text, label); Классы wrappers (обертки) тоже надо логировать.
- Логирование в тестах считают избыточным, потому что:
+Логирование работает под капотом ламбока. Классы, которые нужно логировать помечаются аннотацией @Log4j2, а методы в пейджах,
+ классах wrappers, TestListener помечаются аннотацией  log.info("Writing '{}' in to '{}'", text, label);
+ Логирование в PageTest считают избыточным, потому что:
 -Логи уже есть в PageObject'ах и Wrappers (они описывают каждое действие)
 -TestListener уже логирует начало, успех, падение и время теста
 -Если залогировать каждый шаг в тесте, логи станут слишком громоздкими
@@ -172,13 +176,11 @@ public class TestListener implements ITestListener {
     public void onTestStart(ITestResult iTestResult) {
         log.info("======================================== STARTING TEST {} ========================================%n", iTestResult.getName());
     }
-
     @Override
     public void onTestSuccess(ITestResult iTestResult) {
         log.info("======================================== FINISHED TEST {} Duration: {} ========================================%n",
                 iTestResult.getName(), getExecutionTime(iTestResult));
     }
-
     @Override
     public void onTestFailure(ITestResult iTestResult) {
         log.error("======================================== FAILED TEST {} Duration: {} ========================================%n",
@@ -189,27 +191,22 @@ public class TestListener implements ITestListener {
             AllureUtils.takeScreenshot(driver);
         }
     }
-
     @Override
     public void onTestSkipped(ITestResult iTestResult) {
         log.warn("======================================== SKIPPING TEST {} ========================================%n", iTestResult.getName());
     }
-
     @Override
     public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
         // Не используется
     }
-
     @Override
     public void onStart(ITestContext iTestContext) {
         // Не используется
     }
-
     @Override
     public void onFinish(ITestContext iTestContext) {
         // Не используется
     }
-
     private long getExecutionTime(ITestResult iTestResult) {
         return TimeUnit.MILLISECONDS.toSeconds(iTestResult.getEndMillis() - iTestResult.getStartMillis());
     }
