a1f83ae551ee926781b9199fdfe2a84ebc8da32f
Yakushin <saratov482043162@gmail.com>
2026-05-25 11:15:14 +0400

    [+] lesson8 basetest

--- src/test/java/lesson8/tests/BaseTest.java
+++ src/test/java/lesson8/tests/BaseTest.java
@@ -31,11 +31,12 @@ public class BaseTest {
             chromePrefs.put("credentials_enable_service", false);
             chromePrefs.put("profile.password_manager_enabled", false);
             options.setExperimentalOption("prefs", chromePrefs);
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
             options.addArguments("--incognito");
-            options.addArguments("--disable-notifications");
-            options.addArguments("--disable-popup-blocking");
-            options.addArguments("--disable-infobars");
-            options.addArguments("--headless");
+            options.addArguments("--window-size=1920,1080");
             driver = new ChromeDriver(options);
         } else if (browser.equalsIgnoreCase("edge")) {
             driver = new EdgeDriver();
